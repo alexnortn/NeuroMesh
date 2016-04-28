08dba69cb396248951813b24110266d0f2bf6ead
alexnortn <alexnortn@gmail.com>
2016-04-28 10:21:19 -0400

    final working scripts for TFF neurons

---
+++ bash-scripts/run-process-sac-oodsgc.ps1
@@ -0,0 +1,28 @@
+# Alex Norton 2016
+# Neuro Processor 3.0
+
+# Define paths
+# $inputdirectory = "I:\meshlab\bipolar\"
+$inputdirectory = "I:\Collection\sac\"
+$outputdirectory = "I:\meshlab\oodsgc-ts-low\"
+$meshscript = "E:\GitHub\NeuroMesh\meshlab-scripts\neuro-processor-sac-oodsgc-low-batch.mlx"
+
+# Grab all the items in specified directory
+$items = Get-ChildItem -Path $inputdirectory
+
+# Loop through all items
+foreach ($item in $items) {
+    # Check for obj extension
+    if ($item.extension -eq ".obj") {
+        # Do something
+        Write-Host "Processing" + $item.Name;
+        # Declare I/O files
+        $inputfile = $inputdirectory + $item
+        $outputfile = $outputdirectory + $item.Name;
+        Write-Host "Output:" + $outputfile;
+        # Run Meshlab | Save as obj, only vertex + face normals
+        meshlabserver -i $inputfile -o $outputfile -s $meshscript -om vc vn;
+        Write-Host "Completed" + $item.Name;
+    }
+}
+
