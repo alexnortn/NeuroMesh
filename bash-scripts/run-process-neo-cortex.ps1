33d3b8af6df2ee5f35668a931ea449cfaddda945
alexnortn <alexnortn@gmail.com>
2016-10-04 00:54:53 -0400

    better cortical scripting

--- bash-scripts/run-process-neo-cortex.ps1
+++ bash-scripts/run-process-neo-cortex.ps1
@@ -3,9 +3,9 @@
 
 # Define paths
 # $inputdirectory = "I:\meshlab\bipolar\"
-$inputdirectory = "E:\Drive\Dropbox (MIT)\Projects\Eyewire\FullTime\NEO\pinky"
-$outputdirectory = "E:\Drive\Dropbox (MIT)\Projects\Eyewire\FullTime\NEO\pinky_meshed"
-$meshscript = "E:\GitHub\NeuroMesh\meshlab-scripts\neo_cortex.mlx"
+$inputdirectory = "E:\Drive\Dropbox (MIT)\Projects\Eyewire\FullTime\NEO\piriform_downsample_2\"
+$outputdirectory = "E:\Drive\Dropbox (MIT)\Projects\Eyewire\FullTime\NEO\piriform_downsample_3\"
+$meshscript = "E:\GitHub\NeuroMesh\meshlab-scripts\neo_cortex3_downsample.mlx"
 
 # Grab all the items in specified directory
 $items = Get-ChildItem -Path $inputdirectory
@@ -13,7 +13,7 @@ $items = Get-ChildItem -Path $inputdirectory
 # Loop through all items
 foreach ($item in $items) {
     # Check for obj extension
-    if ($item.extension -eq ".ctm") {
+    if ($item.extension -eq ".obj") {
         # Do something
         Write-Host "Processing" + $item.Name;
         # Declare I/O files
